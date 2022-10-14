a10c31163bae0e17ca2f48db6922f3c72d3a28bb
Christian Schultz <github@schultzzz.net>
2022-10-14 22:51:32 +0200

    Initial version

---
+++ src/Main.java
@@ -0,0 +1,169 @@
+import com.pi4j.Pi4J;
+import com.pi4j.io.gpio.digital.DigitalInput;
+import com.pi4j.io.gpio.digital.DigitalOutput;
+import com.pi4j.io.gpio.digital.DigitalState;
+import com.pi4j.io.gpio.digital.PullResistance;
+import com.pi4j.util.Console;
+
+import org.apache.http.HttpHeaders;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+
+
+    private static final int PIN_SENSOR = 24; // PIN 18 = BCM 24
+    private static final int PIN_LED = 23; // PIN 15 = BCM 22
+
+    private static int sensorCount = 0;
+
+    private static String sendPOST(String url) throws IOException {
+
+        String result = "";
+        HttpPost post = new HttpPost(url);
+
+        // add request parameters or form parameters
+        List<NameValuePair> urlParameters = new ArrayList<>();
+        //urlParameters.add(new BasicNameValuePair("username", "abc"));
+        //urlParameters.add(new BasicNameValuePair("password", "123"));
+        //urlParameters.add(new BasicNameValuePair("custom", "secret"));
+
+        post.setEntity(new UrlEncodedFormEntity(urlParameters));
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+             CloseableHttpResponse response = httpClient.execute(post)){
+
+            result = EntityUtils.toString(response.getEntity());
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        // Create Pi4J console wrapper/helper
+        // (This is a utility class to abstract some of the boilerplate stdin/stdout code)
+        final var console = new Console();
+
+        // Print program title/header
+        console.title("<-- The Pi4J Project -->", "Minimal Example project");
+
+        // ************************************************************
+        //
+        // WELCOME TO Pi4J:
+        //
+        // Here we will use this getting started example to
+        // demonstrate the basic fundamentals of the Pi4J library.
+        //
+        // This example is to introduce you to the boilerplate
+        // logic and concepts required for all applications using
+        // the Pi4J library.  This example will do use some basic I/O.
+        // Check the pi4j-examples project to learn about all the I/O
+        // functions of Pi4J.
+        //
+        // ************************************************************
+
+        // ------------------------------------------------------------
+        // Initialize the Pi4J Runtime Context
+        // ------------------------------------------------------------
+        // Before you can use Pi4J you must initialize a new runtime
+        // context.
+        //
+        // The 'Pi4J' static class includes a few helper context
+        // creators for the most common use cases.  The 'newAutoContext()'
+        // method will automatically load all available Pi4J
+        // extensions found in the application's classpath which
+        // may include 'Platforms' and 'I/O Providers'
+        var pi4j = Pi4J.newAutoContext();
+
+
+
+
+        // ------------------------------------------------------------
+        // Output Pi4J Context information
+        // ------------------------------------------------------------
+        // The created Pi4J Context initializes platforms, providers
+        // and the I/O registry. To help you to better understand this
+        // approach, we print out the info of these. This can be removed
+        // from your own application.
+        // OPTIONAL
+        //PrintInfo.printLoadedPlatforms(console, pi4j);
+        //PrintInfo.printDefaultPlatform(console, pi4j);
+        //PrintInfo.printProviders(console, pi4j);
+
+        // Here we will create I/O interfaces for a (GPIO) digital output
+        // and input pin. We define the 'provider' to use PiGpio to control
+        // the GPIO.
+
+        var ledConfig = DigitalOutput.newConfigBuilder(pi4j)
+                .id("led")
+                .name("LED Flasher")
+                .address(PIN_LED)
+                .shutdown(DigitalState.LOW)
+                .initial(DigitalState.LOW)
+                .provider("pigpio-digital-output");
+        var led = pi4j.create(ledConfig);
+
+
+        var sensorConfig = DigitalInput.newConfigBuilder(pi4j)
+                .id("sensor")
+                .name("Sensor activat")
+                .address(PIN_SENSOR)
+                .pull(PullResistance.PULL_DOWN)
+                .debounce(3000L)
+                .provider("pigpio-digital-input");
+        var sensor = pi4j.create(sensorConfig);
+
+        sensor.addListener(e -> {
+            if (e.state() == DigitalState.LOW) {
+                sensorCount++;
+                console.println("Sensor was activated for the " + sensorCount + "th time");
+
+                led.high();
+                try { Thread.sleep(100 ); } catch (Exception exception) {}
+                led.low();
+
+                try {
+                    sendPOST("www.jp.dk");
+                }
+                catch (IOException ioe) {}
+            }
+        });
+
+        // OPTIONAL: print the registry
+        //PrintInfo.printRegistry(console, pi4j);
+
+
+        while (sensorCount >= 0) {
+            try { Thread.sleep(500 ); } catch (Exception exception) {}
+        }
+
+        // ------------------------------------------------------------
+        // Terminate the Pi4J library
+        // ------------------------------------------------------------
+        // We we are all done and want to exit our application, we must
+        // call the 'shutdown()' function on the Pi4J static helper class.
+        // This will ensure that all I/O instances are properly shutdown,
+        // released by the the system and shutdown in the appropriate
+        // manner. Terminate will also ensure that any background
+        // threads/processes are cleanly shutdown and any used memory
+        // is returned to the system.
+
+        // Shutdown Pi4J
+        pi4j.shutdown();
+    }
+}
